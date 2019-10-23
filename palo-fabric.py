69db79649580aa18719f33548c0a791def90629e
SyrusHCW <syrushcw@gmail.com>
2019-10-23 17:04:15 -0400

    Update palo-fabric.py

--- palo-fabric.py
+++ palo-fabric.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 from pandevice import firewall
 from pandevice import network
 from pandevice import policies
@@ -9,29 +9,34 @@ import pan.xapi
 import sys
 
 #vars
-panorama_dns = '18.214.36.168'
 username = sys.argv[1]
 password = sys.argv[2]
-vlan_id = sys.argv[3]
-eth1_1_prefix = sys.argv[4]
-eth1_1_octet = sys.argv[5]
-eth1_2_prefix = sys.argv[6]
-eth1_2_octet = sys.argv[7]
-template = sys.argv[8]
-virtual_router = sys.argv[9]
+panorama_dns = sys.argv[3]
+template = sys.argv[4]
+vlan_id = sys.argv[5]
+vlan_name = sys.argv[6]
+virtual_router = sys.argv[11]
+interface_id = sys.argv[7]
+interface_prefix = sys.argv[8]
+interface_octet = sys.argv[9]
 vsys_name = sys.argv[10]
-zone_name = sys.argv[11]
+zone_name = sys.argv[6]
+router_id = sys.argv[10]
+present = sys.argv[12]
+ospf_area = sys.argv[13]
+
+
+if present == "false":
+        quit()
 
 panorama_instance = firewall.Firewall(panorama_dns, username, password)
 
-ip_addr1 = '{0}{1}{2}{3}{4}'.format('169.254.', vlan_id, '.', eth1_1_octet, '/30')
-ip_addr2 = '{0}{1}{2}{3}{4}'.format('169.254.', vlan_id, '.', eth1_2_octet, '/30')
+ip_addr1 = '{0}{1}{2}{3}{4}'.format('169.254.', vlan_id, '.', interface_octet, '/30')
 
-interface1 = '{0}{1}{2}'.format('ethernet1/1.', eth1_1_prefix, vlan_id)
-interface2 = '{0}{1}{2}'.format('ethernet1/2.', eth1_2_prefix, vlan_id)
 
-tag1 = '{0}{1}'.format(eth1_1_prefix, vlan_id)
-tag2 = '{0}{1}'.format(eth1_2_prefix, vlan_id)
+interface1 = '{0}{1}{2}{3}'.format(interface_id, '.', interface_prefix, vlan_id)
+
+tag1 = '{0}{1}'.format(interface_prefix, vlan_id)
 
 
 device_template = panorama.Template(template)
@@ -44,24 +49,7 @@ device_template.add(vsys)
 
 subeth = network.Layer3Subinterface(interface1, \
         tag = tag1, \
-        ip = ip_addr1,\
-        mtu = '9178')
-
-vsys.add(subeth)
-
-try:
-	subeth.create()
-except:
-	print('nah')
-	
-subeth.create()
-
-# Create Interface 2
-
-subeth = network.Layer3Subinterface(interface2, \
-        tag = tag2, \
-        ip = ip_addr2,\
-        mtu = '9178')
+        ip = ip_addr1)
 
 vsys.add(subeth)
 
@@ -89,57 +77,31 @@ zone = network.Zone(zone_name, \
 vsys.add(zone)
 zone.create()
 
-#Attach interface2 to routing instance, and zone
+#Attach interface1 to routing instance, and zone
 
-routing_instance = network.VirtualRouter(virtual_router,\
-			interface = interface2)
+routing_ospf = network.Ospf(eanble = True,\
+                        router_id = router_id,\
+                        reject_default_route = False,\
+                        interface = interface1)
 
-vsys.add(routing_instance)
-routing_instance.create()
+vsys.add(routing_ospf)
+routing_ospf.create()
+
+
+routing_area = network.OspfArea(name = ospf_area,\
+                        type = "nssa",\
+                        accept_summary = "true")
+
+vsys.add(routing_area)
+routing_area.create()
+
+
 
 
 zone = network.Zone(zone_name, \
         mode = "layer3", \
-        interface = interface2)
+        interface = interface1)
 
 vsys.add(zone)
 zone.create()
 
-
-# OSPF configuration via API
-
-xapi = pan.xapi.PanXapi(api_username = username, \
-        api_password = password, \
-        hostname= panorama_dns)
-
-# Interface 1
-
-ospf_xml = "<bfd><profile>Inherit-vr-global-setting</profile></bfd><enable>yes</enable><passive>no</passive><authentication>AUTH-OSPF</authentication><gr-delay>10</gr-delay><metric>10</metric><priority>1</priority><hello-interval>3</hello-interval><dead-counts>4</dead-counts><retransmit-interval>5</retransmit-interval><transit-delay>1</transit-delay><link-type><p2p /></link-type>"
-xpath = '{0}{1}{2}{3}{4}{5}{6}'.format("/config/devices/entry[@name='localhost.localdomain']/template/entry[@name='", template, "']/config/devices/entry[@name='localhost.localdomain']/network/virtual-router/entry[@name='", virtual_router, "']/protocol/ospf/area/entry[@name='0.0.0.0']/interface/entry[@name='",interface1, "']")
-
-xapi.set(xpath = xpath, \
-                element = ospf_xml)
-
-
-
-ecmp_xml = "<ecmp><algorithm><ip-hash><use-port>yes</use-port></ip-hash></algorithm><enable>yes</enable><max-path>4</max-path></ecmp>"
-xpath = '{0}{1}{2}{3}{4}'.format("/config/devices/entry[@name='localhost.localdomain']/template/entry[@name='", template, "']/config/devices/entry[@name='localhost.localdomain']/network/virtual-router/entry[@name='", virtual_router, "']")
-
-xapi.set(xpath = xpath, \
-                element = ecmp_xml)
-
-# Interface 2
-
-ospf_xml = "<bfd><profile>Inherit-vr-global-setting</profile></bfd><enable>yes</enable><passive>no</passive><authentication>AUTH-OSPF</authentication><gr-delay>10</gr-delay><metric>10</metric><priority>1</priority><hello-interval>3</hello-interval><dead-counts>4</dead-counts><retransmit-interval>5</retransmit-interval><transit-delay>1</transit-delay><link-type><p2p /></link-type>"
-xpath = '{0}{1}{2}{3}{4}{5}{6}'.format("/config/devices/entry[@name='localhost.localdomain']/template/entry[@name='", template, "']/config/devices/entry[@name='localhost.localdomain']/network/virtual-router/entry[@name='", virtual_router, "']/protocol/ospf/area/entry[@name='0.0.0.0']/interface/entry[@name='",interface2, "']")
-
-xapi.set(xpath = xpath, \
-                element = ospf_xml)
-
-
-
-ecmp_xml = "<ecmp><algorithm><ip-hash><use-port>yes</use-port></ip-hash></algorithm><enable>yes</enable><max-path>4</max-path></ecmp>"
-xpath = '{0}{1}{2}{3}{4}'.format("/config/devices/entry[@name='localhost.localdomain']/template/entry[@name='", template, "']/config/devices/entry[@name='localhost.localdomain']/network/virtual-router/entry[@name='", virtual_router, "']")
-
-xapi.set(xpath = xpath, \
-                element = ecmp_xml)
