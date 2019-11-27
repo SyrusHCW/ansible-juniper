eb2b588a9bd3491f680c853aabff28098aa12b6e
SyrusHCW <syrushcw@gmail.com>
2019-11-27 08:33:58 -0500

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,5 +6,7 @@ lower_var_suffix = var_suffix.lower()
 
 # Format BGP Variable names
 variable_bgp_neighbor_az1 = '{0}{1}{2}'.format('$bgp_neighbor_', lower_var_suffix, '_az1')
+variable_bgp_neighbor_az2 = '{0}{1}{2}'.format('$bgp_neighbor_', lower_var_suffix, '_az2')
 
 print(variable_bgp_neighbor_az1)
+print(variable_bgp_neighbor_az2)
